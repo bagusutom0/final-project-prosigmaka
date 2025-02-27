d338b05da1eeed053423e64a0f6adc9b2e9d4a75
bagusutom0 <bagussajiwoutomo12@gmail.com>
2025-02-27 10:49:40 +0700

    update baseurl

--- app/api/axiosClient.ts
+++ app/api/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:8080/api/v1';
+const baseUrl = 'https://point-of-sales-latest.onrender.com/api/v1';
 
 const axiosClient = axios.create({
   baseURL: baseUrl,
